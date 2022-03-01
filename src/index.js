22b7fc5d57d54faa91bd69880f66a6fb97523126
amyspencerproject <amy@spencerproject.com>
2022-03-01 15:07:30 -0700

    Installed npm so added gitignore file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NameTag from './NameTag';
 
 import "./styles.css";
 
