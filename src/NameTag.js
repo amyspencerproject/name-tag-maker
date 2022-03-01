634e22b28544a84259e86a6587a75f025c4a5fc5
amyspencerproject <amy@spencerproject.com>
2022-03-01 15:38:29 -0700

    Added a simple prop

--- src/NameTag.js
+++ src/NameTag.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 //NameTag is implicit so no curly brackets and no return ()
-const NameTag = () => (
+const NameTag = (props) => (
   <div className='name-tag'>
     <h3 className="title">Hello</h3>
     <p className="subtitle">my name is</p>
-    <h2 className="name">Amy</h2>
+    <h2 className="name">{props.name}</h2>
   </div>
 )
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import "./styles.css";
 const App = () => (
   <div className="App">
     <h1>Name Tag Generator</h1>
+    <NameTag name="Erin" />
+    <NameTag name="Jake" />
+    <NameTag name="Sarah" />
+    <NameTag name="Burt" />
   </div>
 );
 
